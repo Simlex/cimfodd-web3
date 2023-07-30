a449df5405f8851d1d564cc39e845212eefe3dae
Simlex <gettosimlex@gmail.com>
2023-07-30 23:13:22 +0100

    Images constants

---
+++ public/images.ts
@@ -0,0 +1,8 @@
+import logo from "./logo.png";
+import logoBlack from "./logo_black.svg";
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+    logo,
+    logoBlack
+};
