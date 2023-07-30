82cb6e1a6aa2d5c9b98d91a9731cde5b32cadd73
Simlex <gettosimlex@gmail.com>
2023-07-30 23:15:54 +0100

    Created loader

---
+++ src/components/custom/ImageLoader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Loader.module.scss'
+
+const ImageLoader = () => {
+  return (
+    <div>
+        <span className={styles.imageLoader}></span>
+    </div>
+  )
+}
+
+export default ImageLoader
