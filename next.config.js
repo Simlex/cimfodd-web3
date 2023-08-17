6ce2cdc52b61eb6b13f41a743ecb9b487c65e694
Simlex <78862929+Simlex@users.noreply.github.com>
2023-08-17 12:37:25 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     reactStrictMode: true,
     env: {
         // NEXTAUTH_URL: 'http://localhost:3000',
-        NEXTAUTH_URL: 'https://cimfodd-web3.netlify.app',
+        NEXTAUTH_URL: 'https://cimfodd.netlify.app',
         JWT_SECRET: 'SDVEWFQWEQWS',
         APP_NAME: 'Cimfodd',
     },
